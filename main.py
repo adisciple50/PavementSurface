f719e0fa68d0f02ec650b4f3c062ba7889404394
deddokatana <deddokatana@gmail.com>
2016-02-13 12:02:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import shapefile
+
+ba1Area = shapefile.Reader('~/Documents/GeoData/bath-pavement-data/Footways_Ba1.shp')
+bathOuter = shapefile.Reader('~/Documents/GeoData/bath-pavement-data/Footways_BathCityBoundary.shp'
+
+ba1Area.shapeRecords()
